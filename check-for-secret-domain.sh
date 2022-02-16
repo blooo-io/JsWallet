b6a8a162bec1a40f44edbf4e995e66649fc4d36f
Oleksandr Pelykh <olexandr13@gmail.com>
2022-02-16 18:37:23 +0200

    remove extra file

--- check-for-secret-domain.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if grep -Ri veladev222 ./ --exclude-dir node_modules --exclude-dir .compiled --exclude-dir .compiled-ssr --exclude-dir e2e/node_modules --exclude-dir .git; then
-    echo found && exit 1
-else
-    echo not found && exit 2
-fi
-
-echo 'blabla'
