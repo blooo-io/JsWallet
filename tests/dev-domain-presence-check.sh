c4c9e3dce89c67dfac168b886e5353beba08490d
Oleksandr Pelykh <olexandr13@gmail.com>
2022-02-16 18:36:33 +0200

    test

---
+++ tests/dev-domain-presence-check.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# this script checks if development domain is present in repository code
+
+grep -Ri ${{secrets.SECRET_DOMAIN}} . --exclude-dir node_modules --exclude-dir e2e/node_modules --exclude-dir .git
+if [ $? -eq 0 ]
+then
+  echo ' - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -' && \
+  echo 'The development domain was found in repository code.' && \
+  echo 'See filename above. File content is hidden due to security.' && \
+  echo ' - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -' && \
+  exit 13
+else
+  echo "All is fine. Dev domain was not mentioned in current repo."
+  exit 0
+fi
+
